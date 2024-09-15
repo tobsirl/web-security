d9c6d64a398709af8f8ba7473cb137f06b8c3842
Paul Tobin <tobster111@gmail.com>
2024-09-15 10:26:51 +0100

    Signed cookies

--- examples/cookie-jar/index.js
+++ examples/cookie-jar/index.js
@@ -4,8 +4,10 @@ import cookieParser from 'cookie-parser';
 
 import db from './database.js';
 
+const cookieSecret = process.env.COOKIE_SECRET || 'my-secret';
+
 const app = createServer({ cookies: false });
-app.use(cookieParser());
+app.use(cookieParser(cookieSecret));
 
 app.get('/', (req, res) => {
   if (!req.cookies) res.send('Cookies are disabled.');
@@ -19,7 +21,7 @@ app.get('/', (req, res) => {
 app.get('/login', async (req, res) => {
   const loginPage = await readFile('./pages/login.html', 'utf-8');
 
-  if (req.cookies.username) {
+  if (req.signedCookies.username) {
     res.redirect('/profile');
   }
 
@@ -46,6 +48,7 @@ app.post('/login', async (req, res) => {
     res.cookie('username', username, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      signed: true,
     });
     res.redirect('/profile');
   } else {
@@ -62,7 +65,7 @@ app.post('/logout', (_, res) => {
 app.get('/profile', async (req, res) => {
   res.locals.title = 'Profile';
 
-  const username = req.cookies.username;
+  const username = req.signedCookies.username;
 
   if (!username) {
     return res.redirect('/login?error=Please login to view your profile.');
